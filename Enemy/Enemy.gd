95934bcea1a3bc18b4f5abf7b974bc8f70a898f7
jimmyjonezz <jimmyjonezz@bk.ru>
2019-05-15 17:09:39 +0300

    0.3

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -9,13 +9,13 @@ func _ready():
 	pass # Replace with function body.
 	
 func danmaku():
-	for i in range(10):
-		var y = r * cos(i * randi() % 180 / pi)
-		var x = r * sin(i * randi() % 180 / pi)
+	for i in range(3):
+		var y = r * cos(randi() % 180 / pi)#r * cos(i * randi() % 180 / pi)
+		var x = r * cos(randi() % 180 / pi)#r * sin(i + randi() % 180 / pi)
 				
 		var bullet = Bullet.instance()
 		get_parent().add_child(bullet)
-		bullet.start(Vector2( x + position.x , y + position.y ), rand_range(0, 360) * i) 
+		bullet.start(Vector2(position.x + x, position.y + y), 0) 
 
 func _on_Tic_timeout():
 	 danmaku()
