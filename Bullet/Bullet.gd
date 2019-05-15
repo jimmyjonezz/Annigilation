0435df41e01fbd4a289bcfa6f1ce8ed702531433
jimmyjonezz <jimmyjonezz@bk.ru>
2019-05-15 17:47:54 +0300

    0.1.3

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -7,6 +7,7 @@ func start(pos, dir) -> void:
 	rotation = dir
 	position = pos
 	velocity += Vector2(speed, 0).rotated(rotation)
+	$Tic.start()
 
 func _physics_process(delta) -> void:
 	var collision = move_and_collide(velocity * delta)
@@ -15,3 +16,6 @@ func _physics_process(delta) -> void:
 	#	velocity = velocity.bounce(collision.normal)
 		if collision.collider.has_method("hit"):
 			collision.collider.hit()
+
+func _on_Tic_timeout():
+	queue_free()
--- Player/Player.gd
+++ Player/Player.gd
@@ -2,8 +2,6 @@ extends "Character.gd"
 
 class_name Player
 
-var pos = self.position
-
 export var Bullet : PackedScene
 
 func _process(delta) -> void:
