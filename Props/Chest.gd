d9435af44bcd608261ad4b2ea0477ce005da0461
jimmyjonezz <jimmyjonezz@bk.ru>
2019-05-16 16:41:32 +0300

    0.5

---
+++ Props/Chest.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+export var Ammo : PackedScene
+export var Firstkit : PackedScene
+
+func _ready():
+	randomize()
+
+func hit():
+	_spawn_props()
+	queue_free()
+	
+func _spawn_props():
+	var rnd = randi() % 4
+	match rnd:
+		0:
+			return
+		1, 2: 
+			#патроны
+			var Ammo_instance = Ammo.instance()
+			Ammo_instance.set_position(global_position)
+			$"../../SpawnProps/".add_child(Ammo_instance)
+		3: 
+			#аптечка
+			var Firstkit_instance = Firstkit.instance()
+			Firstkit_instance.set_position(global_position)
+			$"../../SpawnProps/".add_child(Firstkit_instance)
